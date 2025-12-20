debbb2991d76c243eb029c3a6f850613aac703e0
Ksawery Margas <ksawerymargas@gmail.com>
2025-12-20 15:29:35 +0100

    Dodano GryDb i migracje bazy danych

---
+++ GraAPI/GryDb.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GraAPI;
+
+public class GryDb : DbContext
+{
+    public GryDb(DbContextOptions<GryDb> options) : base(options)
+    {
+
+    }
+
+    public DbSet<Gry> WszystkieGry { get; set; } = null!;
+}
